9420c603687e40bc1106b4259b5edb17b91874da
DESKTOP-20CI5KE\PC's <mnurhuda009@gmail.com>
2022-10-26 13:41:41 +0700

    update route

--- final-project/routes/routes.go
+++ final-project/routes/routes.go
@@ -1,8 +1,9 @@
 package routes
 
 import (
-	"fgd-golang/final-project/controllers"
+	photoControllers "fgd-golang/final-project/controllers/photo"
 	"fgd-golang/final-project/middlewares"
+	userControllers "fgd-golang/sesi-10/go-jwt/controllers/user"
 
 	"github.com/gin-gonic/gin"
 )
@@ -12,22 +13,22 @@ func StartApp() *gin.Engine {
 
 	userRouter := r.Group("/users")
 	{
-		userRouter.POST("/register", controllers.UserRegister)
-		userRouter.POST("/login", controllers.UserLogin)
+		userRouter.POST("/register", userControllers.UserRegister)
+		userRouter.POST("/login", userControllers.UserLogin)
 
 		userRouter.Use(middlewares.Authentication())
-		userRouter.PUT("/:userId", middlewares.UserAuthorization(), controllers.UpdateUser)
-		userRouter.DELETE("/:userId", middlewares.UserAuthorization(), controllers.DeleteUser)
+		userRouter.PUT("/:userId", middlewares.UserAuthorization(), userControllers.UpdateUser)
+		userRouter.DELETE("/:userId", middlewares.UserAuthorization(), userControllers.DeleteUser)
 	}
 
 	photoRouter := r.Group("/photos")
 	{
 		photoRouter.Use(middlewares.Authentication())
-		photoRouter.POST("/", controllers.CreatePhoto)
-		photoRouter.GET("/", controllers.GetAllPhotos)
+		photoRouter.POST("/", photoControllers.CreatePhoto)
+		photoRouter.GET("/", photoControllers.GetAllPhotos)
 
-		photoRouter.PUT("/:photoId", controllers.UpdatePhoto)
-		photoRouter.DELETE("/:photoId", controllers.DeletePhoto)
+		photoRouter.PUT("/:photoId", photoControllers.UpdatePhoto)
+		photoRouter.DELETE("/:photoId", photoControllers.DeletePhoto)
 	}
 
 	return r
